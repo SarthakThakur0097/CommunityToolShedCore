ade5794f7baf6a72eaef5dd4c31678baab6aff95
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-07-09 15:38:54 -0400

    Added option to change google redirect URL

--- CommunityToolShedCore/Startup.cs
+++ CommunityToolShedCore/Startup.cs
@@ -45,6 +45,7 @@ namespace CommunityToolShedCore
                 {
                     options.ClientId = "7797485727-rlte12cqu3fmf5lqlcdil6agjbermubt.apps.googleusercontent.com";
                     options.ClientSecret = "ILBhyrOsZp8ZLfO9-546zIjo";
+                    options.CallbackPath = "";
                 });
 
             services.AddScoped<IUserRepository, SQLUserRepository>();
